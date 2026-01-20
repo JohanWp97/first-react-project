ee8ecb6018b1dc9bf488efad84a16f2064d4ec2e
johan <johanwp97@gmail.com>
2026-01-19 19:36:38 -0500

    App was created. Components are separated in their files. Taks now are arrays. Each component receives properties and those are called in App.js

---
+++ curso-react-intro-main/src/TodoSearch.js
@@ -0,0 +1,7 @@
+function TodoSearch() {
+    return(
+        <input placeholder="Target 1" />
+    );
+}
+
+export {TodoSearch};
---
+++ curso-react-intro-main/src/TodoTitle.js
@@ -0,0 +1,9 @@
+function TodoTitle({completed, total}) {
+    return(
+        <h1>
+            Has Completado {completed} de {total} Targets
+        </h1>
+    );
+}
+
+export {TodoTitle};
